afdd477d72b55a7eab965aabd96f8952dd91d458
priv <140729444+scriptprivate@users.noreply.github.com>
2024-08-19 19:48:23 -0300

    feat(helper): add Helper module for usage information

---
+++ lib/SecurityGate/Utils/Helper.pm
@@ -0,0 +1,23 @@
+package SecurityGate::Utils::Helper {
+	use strict;
+	use warnings;
+
+	sub new {
+		return "
+            \r\tSecurity Gate v0.0.3
+            \r\tCore Commands
+            \r\t==============
+            \r\tCommand                   Description
+            \r\t-------                   -----------
+            \r\t-t, --token               GitHub token
+            \r\t-r, --repo                GitHub repository
+            \r\t-c, --critical            Critical severity limit
+            \r\t-h, --high                High severity limit
+            \r\t-m, --medium              Medium severity limit
+            \r\t-l, --low                 Low severity limit
+            \r\t--dependency-alerts       Check for dependency alerts
+            \n\n";
+	}
+}
+
+1;
