bce88d8e99c3580e0e160c6d80676cfc18513f66
priv <140729444+scriptprivate@users.noreply.github.com>
2024-08-21 16:53:25 -0300

    update Helper.pm

--- lib/SecurityGate/Utils/Helper.pm
+++ lib/SecurityGate/Utils/Helper.pm
@@ -7,15 +7,17 @@ package SecurityGate::Utils::Helper {
             \r\tSecurity Gate v0.0.3
             \r\tCore Commands
             \r\t==============
-            \r\tCommand                   Description
-            \r\t-------                   -----------
-            \r\t-t, --token               GitHub token
-            \r\t-r, --repo                GitHub repository
-            \r\t-c, --critical            Critical severity limit
-            \r\t-h, --high                High severity limit
-            \r\t-m, --medium              Medium severity limit
-            \r\t-l, --low                 Low severity limit
-            \r\t--dependency-alerts       Check for dependency alerts
+            \r\tCommand                         Description
+            \r\t-------                         -----------
+            \r\t-t, --token                     GitHub token
+            \r\t-r, --repo                      GitHub repository
+            \r\t-c, --critical                  Critical severity limit
+            \r\t-h, --high                      High severity limit
+            \r\t-m, --medium                    Medium severity limit
+            \r\t-l, --low                       Low severity limit
+            \r\t--dependency-alerts             Check for dependency alerts
+            \r\t--secret-scanning-alerts        Check for secret scanning alerts
+            \r\t--code-scanning-alerts          Check for code scanning alerts
             \n\n";
 	}
 }
