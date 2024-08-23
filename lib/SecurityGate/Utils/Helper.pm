b58360987966242a353ce8a543a7f69d6888e4a4
htrgouvea <hi@heitorgouvea.me>
2024-08-23 11:23:02 -0300

    fix identation

--- lib/SecurityGate/Utils/Helper.pm
+++ lib/SecurityGate/Utils/Helper.pm
@@ -1,23 +1,23 @@
 package SecurityGate::Utils::Helper {
-	use strict;
+    use strict;
 	use warnings;
 
 	sub new {
 	      return "
-                  \rSecurity Gate v0.1.0
-                  \rCore Commands
-                  \r====================
-                  \r\tCommand                Description
-                  \r\t-------                -----------
-                  \r\t-t, --token            GitHub token
-                  \r\t-r, --repo             GitHub repository
-                  \r\t-c, --critical         Critical severity limit
-                  \r\t-h, --high             High severity limit
-                  \r\t-m, --medium           Medium severity limit
-                  \r\t-l, --low              Low severity limit
-                  \r\t--dependency-alerts    Check for dependency alerts
-                  \r\t--secret-alerts        Check for secret scanning alerts
-                  \r\t--code-alerts          Check for code scanning alerts\n\n";
+                \rSecurity Gate v0.1.0
+                \rCore Commands
+                \r====================
+                \r\tCommand                Description
+                \r\t-------                -----------
+                \r\t-t, --token            GitHub token
+                \r\t-r, --repo             GitHub repository, organization/repository-name
+                \r\t-c, --critical         Critical severity limit
+                \r\t-h, --high             High severity limit
+                \r\t-m, --medium           Medium severity limit
+                \r\t-l, --low              Low severity limit
+                \r\t--dependency-alerts    Check for dependency alerts
+                \r\t--secret-alerts        Check for secret scanning alerts
+                \r\t--code-alerts          Check for code scanning alerts\n\n";
 	}
 }
 
