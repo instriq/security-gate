b46893c3a40fc35a49465c13339e5476ba856a06
htrgouvea <hi@heitorgouvea.me>
2024-08-22 22:15:04 -0300

    update version and improve identation

--- lib/SecurityGate/Utils/Helper.pm
+++ lib/SecurityGate/Utils/Helper.pm
@@ -3,22 +3,21 @@ package SecurityGate::Utils::Helper {
 	use warnings;
 
 	sub new {
-		return "
-            \r\tSecurity Gate v0.0.3
-            \r\tCore Commands
-            \r\t==============
-            \r\tCommand                         Description
-            \r\t-------                         -----------
-            \r\t-t, --token                     GitHub token
-            \r\t-r, --repo                      GitHub repository
-            \r\t-c, --critical                  Critical severity limit
-            \r\t-h, --high                      High severity limit
-            \r\t-m, --medium                    Medium severity limit
-            \r\t-l, --low                       Low severity limit
-            \r\t--dependency-alerts             Check for dependency alerts
-            \r\t--secret-scanning-alerts        Check for secret scanning alerts
-            \r\t--code-scanning-alerts          Check for code scanning alerts
-            \n\n";
+	      return "
+                  \rSecurity Gate v0.1.0
+                  \rCore Commands
+                  \r====================
+                  \r\tCommand                Description
+                  \r\t-------                -----------
+                  \r\t-t, --token            GitHub token
+                  \r\t-r, --repo             GitHub repository
+                  \r\t-c, --critical         Critical severity limit
+                  \r\t-h, --high             High severity limit
+                  \r\t-m, --medium           Medium severity limit
+                  \r\t-l, --low              Low severity limit
+                  \r\t--dependency-alerts    Check for dependency alerts
+                  \r\t--secret-alerts        Check for secret scanning alerts
+                  \r\t--code-alerts          Check for code scanning alerts\n\n";
 	}
 }
 
